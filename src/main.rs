88837dddb89dcaf7deb55651b5a0f816c93b2767
Phawit Pornwattanakul <me@helloyeew.dev>
2021-06-23 04:38:37 +0700

    Add start script

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use bevy::{input::system::exit_on_esc_system, prelude::*};
 fn main() {
     App::build()
         // Set antialiasing to use 4 samples
-        .add_resource(Msaa { samples: 4 })
+        .insert_resource(Msaa { samples: 4 })
         // Set WindowDescriptor Resource to change title and size
-        .add_resource(WindowDescriptor {
+        .insert_resource(WindowDescriptor {
             title: "Rhythm!".to_string(),
             width: 800.,
             height: 600.,
