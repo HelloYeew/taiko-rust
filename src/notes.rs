b0b10e7080cb69bce27ced6e5afcef1957b20fe4
Phawit Pornwattanakul <me@helloyeew.dev>
2021-06-24 03:59:53 +0700

    Add note method to make it spawn on time

--- src/notes.rs
+++ src/notes.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use crate::consts::*;
+use crate::types::*;
 
 // Keeps the textures and materials for note
 struct NoteMaterialResource {
@@ -26,7 +27,9 @@ impl FromWorld for NoteMaterialResource {
 }
 
 // Note component
-struct Note;
+struct Note {
+    speed: Speed,
+}
 
 // Keep track of time when to spawn a new note
 struct SpawnTimer(Timer);
@@ -34,29 +37,59 @@ struct SpawnTimer(Timer);
 // Spawn notes
 fn spawn_notes(
     mut commands: Commands,
+    mut song_config: ResMut<SongConfig>,
     materials: Res<NoteMaterialResource>,
     time: Res<Time>,
-    mut timer: ResMut<SpawnTimer>,
 ) {
-    if !timer.0.tick(time.delta()).just_finished() {
-        return;
+    // We get the current time since startup (secs) and the time since the last iteration (secs_last),
+    // this way we check if any notes should spawn in this window
+
+    // Set songs to start after 3 seconds
+    let secs = time.seconds_since_startup() - 3.;
+    let secs_last = secs - time.delta_seconds_f64();
+
+    // Counter of how many notes we need to spawn and remove from list
+    let mut remove_counter = 0;
+    for note in &song_config.notes {
+        // List is ordered, so we just check until an item fails
+        // Check if note should be spawned at any point between last frame and this frame
+        if secs_last < note.spawn_time && note.spawn_time < secs {
+            remove_counter += 1;
+
+            // Get the correct material according to speed
+            let material = match note.speed {
+                Speed::Slow => materials.don_texture.clone(),
+                Speed::Medium => materials.don_texture.clone(),
+                Speed::Fast => materials.don_texture.clone(),
+            };
+
+            let mut transform = Transform::from_translation(Vec3::new(SPAWN_POSITION, 150., 1.));
+
+            commands
+                .spawn_bundle(SpriteBundle {
+                    material,
+                    sprite: Sprite::new(Vec2::new(100., 100.)),
+                    transform,
+                    ..Default::default()
+                })
+                .insert(Note {
+                    speed: note.speed,
+                });
+        } else {
+            break;
+        }
     }
 
-    let transform = Transform::from_translation(Vec3::new(SPAWN_POSITION, 0., 1.));
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.don_texture.clone(),
-            sprite: Sprite::new(Vec2::new(140., 140.)),
-            transform,
-            ..Default::default()
-        })
-        .insert(Note);
+    // Remove the arrows we have spawned from the list
+    for _ in 0..remove_counter {
+        song_config.notes.remove(0);
+    }
 }
 
 // Move the notes forward
 fn move_notes(time: Res<Time>, mut query: Query<(&mut Transform, &Note)>) {
-    for (mut transform, _note) in query.iter_mut() {
-        transform.translation.x -= time.delta_seconds() * BASE_SPEED;
+    for (mut transform, note) in query.iter_mut() {
+        transform.translation.x -= time.delta_seconds() * note.speed.value();
     }
 }
 
