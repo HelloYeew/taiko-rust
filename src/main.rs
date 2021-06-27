6922781a215010f5ebbf0b32138b4b9ef53e3a2b
Phawit Pornwattanakul <me@helloyeew.dev>
2021-06-28 03:39:13 +0700

    Add CameraUIBundle for UI

--- src/main.rs
+++ src/main.rs
@@ -33,5 +33,7 @@ fn setup(mut commands: Commands) {
     commands
         .spawn_bundle(OrthographicCameraBundle::new_2d())
         .commands()
+        .spawn_bundle(UiCameraBundle::default())
+        .commands()
         .insert_resource(config);
 }
